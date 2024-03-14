0ad79a11501c44e32d47a9d1e8d0b68f1efebea9
Noor Wachid <wachid@fisip.net>
2024-03-14 17:35:27 +0700

    update wasm

--- binding/javascript/compile.sh
+++ binding/javascript/compile.sh
@@ -3,8 +3,10 @@ CPLUS_INCLUDE_PATH=""
 
 em++ --bind \
 	-std=c++11 \
-	-O3 \
 	-Isource \
+	-O2 \
 	-s NO_FILESYSTEM=1 \
+	-s ALLOW_MEMORY_GROWTH=1 \
 	-o binding/javascript/gularen.js \
 	binding/javascript/binding.cpp
+
