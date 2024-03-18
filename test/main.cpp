28f9d5e29bcad1ba0d6ad7cf9ba86d80c2e4750d
Noor Wachid <wachid@fisip.net>
2024-03-18 21:07:38 +0700

    refactor test

--- test/main.cpp
+++ test/main.cpp
@@ -1,77 +1,63 @@
 #include "Gularen/Frontend/Parser.h"
 #include "Gularen/Frontend/Node.h"
-#include "Gularen/Library/String.h"
-#include <stdio.h>
 
 using namespace Gularen;
 
 void print(Node* node, unsigned int depth = 0) {
 	for (unsigned int i = 0; i < depth; i += 1) {
-		printf("  ");
+		std::cout << "  ";
 	}
 	node->print();
 	for (unsigned int i = 0; i < node->children.size(); i += 1) {
-		print(node->children.get(i), depth + 1);
+		print(node->children[i], depth + 1);
 	}
 }
 
-String readFile(const char* path) {
-	String content;
-	FILE* file = fopen(path, "r");
-
-	if (file == nullptr) {
-		printf("failed to open file %s\n", path);
-		return String();
-	}
-
-	fseek(file, 0, SEEK_END);
-	char* data = content.expand(ftell(file));
-	fseek(file, 0, SEEK_SET);
-
-	fread(data, content.size(), sizeof(char), file);
-	fclose(file);
-
+std::string readFile(const char* path) {
+	std::string content(std::filesystem::file_size(path), '\0');
+	std::ifstream file(path);
+	file.read(content.data(), content.size());
 	return content;
 }
 
 int main(int argc, char** argv) {
 	if (argc < 2) {
-		printf("please specify the file path\n");
+		std::cout << "please specify the file path\n";
 		return 1;
 	}
 
 	if (argc < 3) {
-		printf("please specify the action [parse|expect]\n");
+		std::cout << "please specify the action [parse|expect]\n";
 		return 1;
 	}
 
-	String content = readFile(argv[2]);
-	StringSlice contentSlice(content.pointer(), content.size());
+	std::string content = readFile(argv[2]);
+	std::string_view contentSlice(content.data(), content.size());
 
 	Parser parser;
 	Document* document = parser.parse(contentSlice);
 
 	if (document->children.size() < 2 || 
-		document->children.get(0)->kind != NodeKind::codeBlock ||
-		document->children.get(1)->kind != NodeKind::codeBlock
+		document->children[0]->kind != NodeKind::codeBlock ||
+		document->children[1]->kind != NodeKind::codeBlock
 	) {
-		printf("invalid test file\n");
+		std::cout << "invalid test file\n";
 		return 0;
 	}
 
-	if (StringSlice(argv[1]) == "parse") {
+	if (std::string_view(argv[1]) == "parse") {
 		Parser parser;
-		StringSlice content = static_cast<CodeBlock*>(document->children.get(0))->content;
+		std::string_view content = static_cast<CodeBlock*>(document->children[0])->content;
 		Document* contentDocument = parser.parse(content);
 		if (contentDocument != nullptr) {
 			print(contentDocument);
 		}
 	}
 
-	if (StringSlice(argv[1]) == "expect") {
+	if (std::string_view(argv[1]) == "expect") {
 		Parser parser;
-		StringSlice content = static_cast<CodeBlock*>(document->children.get(1))->content;
-		printf("%.*s\n", content.size(), content.pointer());
+		std::string_view content = static_cast<CodeBlock*>(document->children[1])->content;
+		std::cout << content << "\n";
 	}
 
 	return 0;
